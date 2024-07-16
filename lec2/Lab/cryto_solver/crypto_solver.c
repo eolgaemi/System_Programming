7bc2eee4eb0c61375d303741321c4bb9cddb43ac
eolgaemi <yjwlm10@daum.net>
2024-07-16 21:16:09 +0900

    0716_done

---
+++ lec2/Lab/cryto_solver/crypto_solver.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[]){
+	
+	int rfd
+	char buffer[1];
+	
+	rfd = open("en.txt",O_RDONLY,0644);
+
+
+	
+
+	return 0;
+}
